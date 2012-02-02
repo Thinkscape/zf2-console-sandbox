24faea997062c6829ebd16c0167ad178ab27c593
Thinkscape <abodera@gmail.com>
2012-02-02 13:52:47 +0100

    Add route definition parsing to Mvc\Router\Cli\Simple

--- demo/route.php
+++ demo/route.php
@@ -2,23 +2,28 @@
 use Zend\Http\Request as Request,
     Zend\Stdlib\Request as BaseRequest,
     Zend\Mvc\Router\Cli\RouteMatch,
-    Zend\Console\Getopt
+    Zend\Console\Getopt,
+    Zend\Mvc\Router\Cli\Simple
 //    Zend\Mvc\Router\Cli\Simple
 ;
 require_once '_init.php';
 
+$route = Simple::factory(array(
+    'route' => 'install ( module | package | application | app) NAME',
+    'route' => 'delete NAME [MODE]',
+));
 
-$g = new Getopt(
-    array(
-        'lines=s' => 'Lines-option'
-    ),
-    null,
-    array(
-        Getopt::CONFIG_FREEFORM_FLAGS   => true,
-        Getopt::CONFIG_PARSEALL         => true ,
-    )
-);
-$g->parse();
-print_r($g->getArguments());
 
-//$route = new Zend\Cli\Route();
+//$g = new Getopt(
+//    array(
+//        'lines=s' => 'Lines-option'
+//    ),
+//    null,
+//    array(
+//        Getopt::CONFIG_FREEFORM_FLAGS   => true,
+//        Getopt::CONFIG_PARSEALL         => true ,
+//    )
+//);
+//$g->parse();
+//print_r($g->getArguments());
+
