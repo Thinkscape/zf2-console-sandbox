334abc5cbf71916eb8fe6a6b71ac984d3eb57981
Thinkscape <abodera@gmail.com>
2012-02-07 14:57:58 +0100

    Add matching on non-positional params, fix route parsing

--- demo/route.php
+++ demo/route.php
@@ -1,22 +1,26 @@
 <?php
-use Zend\Http\Request as Request,
-    Zend\Stdlib\Request as BaseRequest,
+use Zend\Stdlib\Request as BaseRequest,
     Zend\Mvc\Router\Cli\RouteMatch,
     Zend\Console\Getopt,
     Zend\Mvc\Router\Cli\Simple,
     Zend\Cli\Request
 //    Zend\Mvc\Router\Cli\Simple
 ;
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
 require_once '_init.php';
 
+
 $route = Simple::factory(array(
-    'route' => 'install ( module | package | application | app) NAME',
-    'route' => 'delete NAME [MODE]',
+//    'route' => 'install ( module | package | application | app) NAME',
+    'route' => '--foo --bar=n IMPORTANT',
 ));
 $request = new Request();
 $match = $route->match($request);
 
 print_r($match);
+
 
 
 //$g = new Getopt(
@@ -25,10 +29,11 @@ print_r($match);
 //    ),
 //    null,
 //    array(
-//        Getopt::CONFIG_FREEFORM_FLAGS   => true,
-//        Getopt::CONFIG_PARSEALL         => true ,
+////        Getopt::CO
+////        Getopt::CONFIG_FREEFORM_FLAGS   => true,
+//        Getopt::CONFIG_PARSEALL         => false ,
 //    )
 //);
 //$g->parse();
 //print_r($g->getArguments());
-
+//
